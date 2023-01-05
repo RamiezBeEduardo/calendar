bdb6b0c4a65101ba5f3ef9fd108f74876c4a7299
Mateo <sanromanxp@gmail.com>
2023-01-05 16:35:21 -0500

    A

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,19 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './pages/user'
+import User from './pages/user'
+import Oper from './pages/oper'
 import './index.css'
+import { Routes, Route, Outlet, NavLink } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom'
+
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+      <BrowserRouter>
+      <Routes>
+          <Route path="user" element={<User />} />
+          <Route path="oper" element={<Oper />} />
+      </Routes>
+      </BrowserRouter>
+  </React.StrictMode>
 )
